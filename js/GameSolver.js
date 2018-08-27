331f560f129b541446b83b8c902b2b44697e319b
Fabian Michel <fabian-michel@gmx.de>
2018-08-27 15:59:56 +0200

    Updated GameSolver
    There is now a function that can solve levels using a given
    level description object

--- js/GameSolver.js
+++ js/GameSolver.js
@@ -21,6 +21,26 @@ const DEPTH_LIMIT = 2;
 const FOUND_SOLUTION = 3;
 
 /**
+ * Solve a level with a given description
+ * @param {object} obj a level description object
+ * @param {number} [maxTime] the maximum running time in milliseconds
+ * @param {number} [maxDepth] the maximum move depth
+ * @return {any[]} an array: [solver, [retVal, statesSeen, lastDepth, solMoves, solMoveIdx]] where:
+ * - solver is an instance of GameSolver for the given level
+ * - retVal is one of NOT_POSSIBLE, TIME_LIMIT, DEPTH_LIMIT, FOUND_SOLUTION
+ * - statesSeen contains the number of states that were visited
+ * - lastDepth contains the move depth of the last state that was visited
+ * - solMoves contains the number of moves needed to solve this state (in case of FOUND_SOLUTION)
+ * - solMoveIdx contains the last move that was made as an index of movesArray (making reconstruction
+ *   possible) (in case of FOUND_SOLUTION)
+ */
+function solveFromLevelDescription(obj, maxTime = 10000, maxDepth = Infinity) {
+  const solver = new GameSolver(new GameState(obj.board));
+  return [solver, solver.solve(obj.fallThrough ? true : false, obj.changeGravity ? true : false,
+    obj.options || {}, maxTime, maxDepth)];
+}
+
+/**
  * This class tries to solve Snakebird games by brute force
  */
 class GameSolver {
@@ -79,7 +99,13 @@ class GameSolver {
    * Attempt to solve the game state
    * @param {boolean} [fallThrough] if set to true, snakes and objects that fall out of the board
    * will appear again at the top
-   * @param {number[]} [gravity] the direction of gravity (one of LEFT, RIGHT, UP, DOWN)
+   * @param {boolean} [changeGravity] whether to change the direction of gravity in clockwise order
+   * when the snake eats a fruit
+   * @param {object} [options] additional options to be taken into account when calculating the next state
+   * @param {boolean} [options.allowMovingWithoutSpace] if set to true, a snake can move without space
+   * if the object blocking its path is moved at the same time
+   * @param {boolean} [options.allowTailBiting] if allowMovingWithoutSpace is set to true, but this
+   * parameter is set to false, a snake can move without space if it is not blocking itself
    * @param {number} [maxTime] the maximum running time in milliseconds
    * @param {number} [maxDepth] the maximum move depth
    * @return {number[]} an array: [retVal, statesSeen, lastDepth, solMoves, solMoveIdx] where:
@@ -90,7 +116,9 @@ class GameSolver {
    * - solMoveIdx contains the last move that was made as an index of movesArray (making reconstruction
    *   possible) (in case of FOUND_SOLUTION)
    */
-  solve(fallThrough = false, gravity = DOWN, maxTime = 10000, maxDepth = Infinity) {
+  solve(fallThrough = false, changeGravity = false, options = {}, maxTime = 10000, maxDepth = Infinity) {
+    const { allowMovingWithoutSpace = false, allowTailBiting = false } = options;
+    options = { allowMovingWithoutSpace, allowTailBiting };
     const posMove = [LEFT, RIGHT, UP, DOWN];
     const startTime = (new Date()).getTime();
     let retVal = NOT_POSSIBLE; let solMoves = -1; let lastDepth = -1; let solMoveIdx = -1; let statesSeen = 0;
@@ -103,7 +131,7 @@ class GameSolver {
       for (let i=0; i<cState.snakes.length && retVal != FOUND_SOLUTION; i++) { // all snakes can be moved
         for (let k=0; k<posMove.length && retVal != FOUND_SOLUTION; k++) { // every snake can be moved in every direction
           let nState = cState.clone();
-          nState = gameTransition(nState, i, posMove[k], fallThrough, gravity, false); // calculate next state
+          nState = gameTransition(nState, i, posMove[k], fallThrough, nState.gravity, false, changeGravity, options); // calculate next state
           if (nState !== null) { // move was valid
             lastDepth = mDepth + 1;
             if (this.addToStateQueue(nState, this.movesArray.length, mDepth + 1)) { // returns true if state wasn't seen before
--- test/GameSolver.test.js
+++ test/GameSolver.test.js
@@ -2,7 +2,7 @@
 function GameSolverTest() {
   for (let i=0; i<8; i++) {
     const gs = new GameSolver(new GameState(stateSamples[i]));
-    const res = gs.solve(false, DOWN, 1000);
+    const res = gs.solve(false, false, {}, 1000);
     assertStrictEqual(stdSolutions[i][0], res[0]);
     if (res[0] == FOUND_SOLUTION)
       assertStrictEqual(stdSolutions[i][1], res[3]);
